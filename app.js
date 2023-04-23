92648fbd64559f8da58b9e808603eac96dc63d9e
MalS <90wide@gmail.com>
2023-04-23 17:05:26 +0300

    refactor(base-3.1): use uniform naming style for selectors

--- app.js
+++ app.js
@@ -7,10 +7,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput = document.getElementById('task-new') //Add a new task.
+var taskInput = document.getElementById('todo__text') //Add a new task.
 var addButton = document.getElementsByTagName('button')[0] //first button
 var incompleteTaskHolder = document.getElementById('todo__list') //ul of #incompleteTasks
-var completedTasksHolder = document.getElementById('completed-tasks') //completed-tasks
+var completedTasksHolder = document.getElementById('todo__list_complete') //completed-tasks
 
 //New task list item
 var createNewTaskElement = function (taskString) {
@@ -35,13 +35,14 @@ var createNewTaskElement = function (taskString) {
 
     //Each elements, needs appending
     checkBox.type = 'checkbox'
+    checkBox.classList.add('todo__checkbox')
     editInput.type = 'text'
-    editInput.className = 'task'
+    editInput.className = 'todo__text'
 
     editButton.innerText = 'Edit' //innerText encodes special characters, HTML does not.
-    editButton.className = ' button edit'
+    editButton.className = ' button button_edit'
 
-    deleteButton.className = 'button delete'
+    deleteButton.className = 'button button_delete'
     deleteButtonImg.src = './remove.svg'
     deleteButtonImg.classList.add('button__img')
     deleteButton.appendChild(deleteButtonImg)
@@ -78,7 +79,7 @@ var editTask = function () {
 
     var editInput = listItem.querySelector('input[type=text]')
     var label = listItem.querySelector('label')
-    var editBtn = listItem.querySelector('.edit')
+    var editBtn = listItem.querySelector('.button_edit')
     var containsClass = listItem.classList.contains('todo__task_editMode')
     //If class of the parent is .editmode
     if (containsClass) {
@@ -142,8 +143,8 @@ var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
     console.log('bind list item events')
     //select ListItems children
     var checkBox = taskListItem.querySelector('input[type=checkbox]')
-    var editButton = taskListItem.querySelector('button.edit')
-    var deleteButton = taskListItem.querySelector('button.delete')
+    var editButton = taskListItem.querySelector('.button_edit')
+    var deleteButton = taskListItem.querySelector('.button_delete')
 
     //Bind editTask to edit button.
     editButton.onclick = editTask
